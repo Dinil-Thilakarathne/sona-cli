56fb5183cd953a1732a3d63abbe603236a35d0d2
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-05-08 14:58:44 +0530

    feat: add Astro + Svelte template option to project creation

--- utils/create-projects.ts
+++ utils/create-projects.ts
@@ -1,7 +1,6 @@
 import inquirer from "inquirer";
 import { execa } from "execa";
 import chalk from "chalk";
-import fs from "fs/promises";
 
 async function createProject() {
   const { template } = await inquirer.prompt([
@@ -9,7 +8,7 @@ async function createProject() {
       name: "template",
       type: "list",
       message: "Choose a template:",
-      choices: ["Next.js + TypeScript (latest)", "Cancel"],
+      choices: ["Next.js + TypeScript (latest)", "Astro + Svelte", "Cancel"],
     },
   ]);
 
@@ -30,15 +29,37 @@ async function createProject() {
 
   if (template === "Next.js + TypeScript (latest)") {
     // Run bun create next
-    await execa("bunx", [
-      "create-next-app",
+    await execa(
+      "bunx",
+      [
+        "create-next-app@latest",
+        projectName,
+        "--typescript",
+        "--eslint",
+        "--tailwind",
+        "--app",
+        "--src-dir",
+      ],
+      {
+        stdio: "inherit",
+      }
+    );
+
+    console.log(chalk.green(`\n✅ Project created at ./${projectName}`));
+  } else if (template === "Astro + Svelte") {
+    // init Astro project
+    await execa("pnpm", [
+      "create",
+      "astro@latest",
       projectName,
+      "--",
+      "--template",
+      "minimal",
       "--typescript",
-      "--eslint",
-      "--tailwind",
-      "--app",
-      "--src-dir",
-    ], {
+      "strict",
+      "--git",
+      "--install",
+    ],{
       stdio: "inherit",
     });
 
